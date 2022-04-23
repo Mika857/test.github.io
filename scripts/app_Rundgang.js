145efb2067cea36cafdf6c3dee90df6854732d6e
Mika857 <myykami@pm.me>
2022-04-23 20:25:52 +0200

    2.4.5.2

--- scripts/app_Rundgang.js
+++ scripts/app_Rundgang.js
@@ -1,12 +1,16 @@
 const isMobile = ('ontouchstart' in document.documentElement && navigator.userAgent.match(/Mobi/));
 
 if(/Mobi/.test(navigator.userAgent) && isMobile){
-    var doc = window.document;
-    var el = doc.documentElement;
+    // var doc = window.document;
+    // var el = doc.documentElement;
 
-    var requestFullScreen = el.requestFullscreen || el.mozRequestFullScreen || el.webkitRequestFullScreen || el.msRequestFullscreen;
+    // var requestFullScreen = el.requestFullscreen || el.mozRequestFullScreen || el.webkitRequestFullScreen || el.msRequestFullscreen;
 
-    requestFullScreen.call(el);
+    // requestFullScreen.call(el);
+
+    var el =document.documentElement;
+
+    el.requestFullscreen();
 
     window.location = "Rundgang/Handy/output/Aula.html";
 }
